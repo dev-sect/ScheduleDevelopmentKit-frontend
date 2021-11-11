28676a0a15ee777071368f9066e122ce72e5cad3
Alexander <sashafromlibertalia@gmail.com>
2021-11-11 20:01:21 +0300

    fix: fixed vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,22 @@
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
+    configureWebpack: () => {
+        return {
+            resolve: {
+                extensions: ['.web.js', '.vue', '.js', '.json', '.less', '.scss', '.sass'],
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.less$/,
+                        use: [
+                            'vue-style-loader',
+                            'css-loader',
+                            'less-loader'
+                        ],
+                    },
+                ],
+            },
+        };
     },
+
 }
