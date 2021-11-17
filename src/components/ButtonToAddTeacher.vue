b77eecdba9e7d0a7475b791c251dc655d81d887a
Semyon <miht.sem@yandex.com>
2021-11-17 18:35:43 +0300

    feat: add new component ButtonToAddTeacher

---
+++ src/components/ButtonToAddTeacher.vue
@@ -0,0 +1,18 @@
+<template>
+	<button class='button-add-teacher'>{{textButton}}</button>
+</template>
+
+<script>
+export default {
+	name: "ButtonToAddTeacher",
+	data() {
+	return {
+		textButton: 'Добавить'
+		}
+	}
+}
+</script>
+
+<style scoped lang="scss">
+@import '../assets/styles/components/button-teacher.scss';
+</style>
