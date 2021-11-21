a49d4d0a500929ba597ba89555e78256073eb674
Aleksey <333489@niuitmo.ru>
2021-11-21 17:26:21 +0300

    feat: add more types for checking

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -2,8 +2,9 @@
 
   <input
       :type="type"
-      v-on:click="setType">
-
+      v-on:click="setType"
+      v-on:change="checkCorrectValue"
+      v-model="value">
 </template>
 
 <script>
@@ -14,22 +15,56 @@ export default {
   data() {
     return {
       type: "text",
+      value: '',
     }
   },
 
   methods: {
+
     setType() {
-      console.log(this);
       if (this.$el.classList.contains('password')) {
         this.type = 'password';
       } else {
         this.type = 'text';
       }
+    },
+
+    checkCorrectValue() {
+      if (this.$el.classList.contains('phone')) {
+        let phoneRegular = /(\+7|8)[\s(]?(\d{3})[\s)]?(\d{3})[\s-]?(\d{2})[\s-]?(\d{2})/g;
+        if (phoneRegular.test(this.value)) {
+          if (this.$el.classList.contains('errorInput')) this.$el.classList.remove('errorInput');
+        } else {
+          this.$el.classList.add('errorInput');
+        }
+      }
+
+      if (this.$el.classList.contains('e-mail')) {
+        let emailRegular = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        if (emailRegular.test(this.value)) {
+          if (this.$el.classList.contains('errorInput')) this.$el.classList.remove('errorInput');
+        } else {
+          this.$el.classList.add('errorInput');
+        }
+      }
     }
+
   },
 }
+
+/*
+В общем-то:
+phone - для телефонов
+e-mail - для почт
+password - для паролей
+ */
+
+
 </script>
 
 <style scoped lang="scss">
 @import '../assets/styles/components/Input.scss';
 </style>
+
+
+
