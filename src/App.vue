d17fc9245e395b73a016232ac494e80efc9c5f70
Aleksey <raft.took@yandex.ru>
2021-11-21 03:00:11 +0300

    feat: add input

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
+  <div class="div">
     <ButtonToAddTeacher></ButtonToAddTeacher>
+    <Input class="e-mail" placeholder="example@itmo.ru"/>
+    <Input class="password" placeholder="Ваш пароль"/>
+  </div>
 </template>
 
 <script>
 import ButtonToAddTeacher from './components/ButtonToAddTeacher.vue'
+import Input from "./components/Input.vue" ;
 
 export default {
-    name: 'App',
-    components: {
-        ButtonToAddTeacher
-    }
+  name: 'App',
+  components: {
+    Input,
+    ButtonToAddTeacher
+  }
 }
 </script>
 
---
+++ src/components/Input.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <input
+      :type="type"
+      v-on:click="setType">
+
+</template>
+
+<script>
+export default {
+
+  name: "Input",
+
+  data() {
+    return {
+      type: "text",
+    }
+  },
+
+  methods: {
+    setType() {
+      console.log(this);
+      if (this.$el.classList.contains('password')) {
+        this.type = 'password';
+      } else {
+        this.type = 'text';
+      }
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import '../assets/styles/components/Input.scss';
+</style>
