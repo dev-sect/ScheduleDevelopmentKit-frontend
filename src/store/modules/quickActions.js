4cc53381bf467ed9226268c8de34b38667b25453
Semyon <miht.sem@yandex.com>
2021-11-21 21:34:08 +0300

    fix: update formatting x2

---
+++ src/store/modules/quickActions.js
@@ -0,0 +1,13 @@
+const TypeCards = Object.freeze({
+    TIMETABLE: {
+        name: "Timetable",
+        data: {
+            backgroundColor: '$quick__btn-background-5',
+            backgroundColorHover: '$quick__btn-background-5-hover',
+            textHeading: 'Расписание',
+            describeText: 'Можно увидеть данные о студентах и преподавателях',
+            icon: '../icons/buttonOfQuickAction/timetable.svg'
+        }
+    },
+})
+export default TypeCards;
