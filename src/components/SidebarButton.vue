b915780fdfd59590fe8c60807fef7507c9e477e6
dima-prezhentsov <dimonpre@gmail.com>
2021-11-30 00:55:20 +0300

    feat: styles in separate file

--- src/components/SidebarButton.vue
+++ src/components/SidebarButton.vue
@@ -33,47 +33,5 @@ export default {
 </script>
 
 <style scoped lang="scss">
-@import "../assets/styles/helpers/variables";
-@import "../assets/styles/helpers/typography";
-@import "../assets/styles/base/common";
-@import "../assets/styles/helpers/visual";
-
-.sidebar-btn {
-  // Temporary margin
-  margin: 30px 300px;
-  //
-  width: 18.7rem;
-  height: 3.5rem;
-  background-color: $background-color;
-  @include textPlain;
-  font-weight: bold;
-  font-size: 1.5rem;
-  color: $text__default-color--gray;
-  //background-color: $background-color;
-  display: flex;
-  align-items: center;
-
-  &:hover {
-    background-color: $item__state-background--blur;
-  }
-
-  img {
-    color: $text__default-color--gray;
-    fill: $text__default-color--gray;
-    margin-left: 3.7rem;
-  }
-  p {
-    margin: 0 0 0 1rem;
-  }
-}
-.selected {
-  color: $university__brand-color;
-  &::before {
-    content: "";
-    border-color: $university__brand-color;
-    border-style: solid;
-    border-bottom-width: 3.4rem;
-    border-radius: 0 $borderRadius $borderRadius 0;
-  }
-}
+@import "../assets/styles/components/sidebar-btn";
 </style>
