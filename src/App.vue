1fa6918a7751d2bb00a5f25db05ac68258113441
Alexander <sashafromlibertalia@gmail.com>
2022-01-09 20:06:34 +0300

    test: added empty test script

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="app">
         <ButtonToAddTeacher></ButtonToAddTeacher>
         <QuickAction type="AddTimetable"></QuickAction>
         <QuickAction type="NewEvent"></QuickAction>
