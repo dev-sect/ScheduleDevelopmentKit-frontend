9df16ce2d11a3508784c5693249b54d40ce5d995
LewinOs <raft.took@yandex.ru>
2022-01-19 14:19:29 +0300

    feat: add more types of enums

--- src/store/enums/input-data-enums.js
+++ src/store/enums/input-data-enums.js
@@ -1,30 +1,45 @@
 const inputTypes = Object.freeze({
     PASSWORD: "password",
     EMAIL: "e-mail",
-    PHONE: "phone"
+    PHONE: "phone",
+    LASTNAME: 'lastname',
+    NAME: 'name',
+    PATRONYMIC: 'patronymic',
 });
 
 const regular = Object.freeze({
-    phone: /(\+7|8)[\s(]?(\d{3})[\s)]?(\d{3})[\s-]?(\d{2})[\s-]?(\d{2})/,
-    email:/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    password:/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+    PHONE: /(\+7|8)[\s(]?(\d{3})[\s)]?(\d{3})[\s-]?(\d{2})[\s-]?(\d{2})/,
+    EMAIL:/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    PASSWORD:/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+    LASTNAME: /.?/,
+    NAME: /.?/,
+    PATRONYMIC: /.?/
 })
 
 const errorMessage =Object.freeze( {
-    phone: 'Введите корректный номер телефона',
-    email: 'Введите корректный e-mail',
-    password: 'Пароль должен содержать: минимум 8 символов, хотя бы одну цифру'
+    PHONE: 'Введите корректный номер телефона',
+    EMAIL: 'Введите корректный e-mail',
+    PASSWORD: 'Пароль должен содержать: минимум 8 символов, хотя бы одну цифру',
+    LASTNAME: '',
+    NAME: '',
+    PATRONYMIC: ''
 })
 
 const placeHolder = Object.freeze({
-    phone: '+79999999999',
-    email: 'example@itmo.ru',
-    password: 'Ваш пароль'
+    PHONE: '+79999999999',
+    EMAIL: 'example@itmo.ru',
+    PASSWORD: 'Ваш пароль',
+    LASTNAME: 'Фамилия',
+    NAME: 'Имя',
+    PATRONYMIC: 'Отчество'
 })
 
 const headValue = Object.freeze({
-    phone: "Телефон",
-    email: "E-mail",
-    password: "Пароль"
+    PHONE: "Телефон",
+    EMAIL: "E-mail",
+    PASSWORD: "Пароль",
+    LASTNAME: 'ФИО',
+    NAME: '',
+    PATRONYMIC: ''
 })
 export {inputTypes, regular, errorMessage, placeHolder, headValue};
