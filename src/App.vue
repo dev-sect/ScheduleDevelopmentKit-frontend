be81bb80e79a7a9b4c5e392784789dac072ae0a1
dima-prezhentsov <dimonpre@gmail.com>
2022-02-08 18:19:08 +0300

    feat: initial commit for EditComponent Это первая версия, так как я костыльнул там немного

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,28 @@
 <template>
     <div id="app">
-      <div class="pagesLayout">
-        <div class="pagesLayout_contentLeft">
-          <SideBar v-if = "$route.path.indexOf('/auth')==-1"/>
-        </div>
-        <div class="pagesLayout_contentRight">
-          <HeaderBar v-if = "$route.path.indexOf('/auth')==-1"/>
-          <router-view></router-view>
-        </div>
-      </div>
+<!--      <div class="pagesLayout">-->
+<!--        <div class="pagesLayout_contentLeft">-->
+<!--          <SideBar v-if = "$route.path.indexOf('/auth')==-1"/>-->
+<!--        </div>-->
+<!--        <div class="pagesLayout_contentRight">-->
+<!--          <HeaderBar v-if = "$route.path.indexOf('/auth')==-1"/>-->
+<!--          <router-view></router-view>-->
+<!--        </div>-->
+<!--      </div>-->
+      <Edit></Edit>
     </div>
 </template>
 
 <script>
 
-import SideBar from "@/components/SideBar";
-import HeaderBar from "@/components/HeaderBar";
+// import SideBar from "@/components/SideBar";
+// import HeaderBar from "@/components/HeaderBar";
+import Edit from "@/components/Edit";
 
 export default {
     name: 'App',
     components: {
-      HeaderBar,
-      SideBar
+      Edit
     },
 }
 </script>
