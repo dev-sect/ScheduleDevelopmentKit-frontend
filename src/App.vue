b83b6b3df15307e1d740ca50f2c70854f508c502
dima-prezhentsov <dimonpre@gmail.com>
2022-02-14 13:59:51 +0300

    fixed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
     <div id="app">
-<!--      <div class="pagesLayout">-->
-<!--        <div class="pagesLayout_contentLeft">-->
-<!--          <SideBar v-if = "$route.path.indexOf('/auth')==-1"/>-->
-<!--        </div>-->
-<!--        <div class="pagesLayout_contentRight">-->
-<!--          <HeaderBar v-if = "$route.path.indexOf('/auth')==-1"/>-->
-<!--          <router-view></router-view>-->
-<!--        </div>-->
-<!--      </div>-->
-      <Edit @change="change"></Edit>
+      <div class="pagesLayout">
+        <div class="pagesLayout_contentLeft">
+          <SideBar v-if = "$route.path.indexOf('/auth')===-1"/>
+        </div>
+        <div class="pagesLayout_contentRight">
+          <HeaderBar v-if = "$route.path.indexOf('/auth')===-1"/>
+          <router-view></router-view>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
 
-// import SideBar from "@/components/SideBar";
-// import HeaderBar from "@/components/HeaderBar";
-import Edit from "@/components/Edit";
+ import SideBar from "@/components/SideBar";
+ import HeaderBar from "@/components/HeaderBar";
 
 export default {
     name: 'App',
     components: {
-      Edit
+      SideBar,
+      HeaderBar
     },
     methods: {
       change() {
